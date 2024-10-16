367bbed6acfed30327f9a02e282f9383372958a0
RandyRagnar <randytsiory@gmail.com>
2024-10-16 09:27:43 +0300

    change sentence for best

--- src/views/Mangas.vue
+++ src/views/Mangas.vue
@@ -26,10 +26,10 @@
       <div class="row">
         <div class="col-12">
           <h4 class="mb-2 text-center text-white">
-            Veuillez noter que cette page peut prendre un certain temps à se recharger en raison
+            Veuillez noter que cette page peut prendre un certain temps à se recharger en raison de son status
           </h4>
           <h4 class="mb-5 text-center text-white">
-            de la latence du serveur. En attendant, n'hésitez pas à consulter les actualités du jour
+            "sleepless", sur un hote gratuit. En attendant, n'hésitez pas à consulter les actualités du jour
           </h4>
 
           <h4 class="text-center text-white">Chargement en cours...</h4>
