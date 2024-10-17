46b976f96d430b21cdc145e05b4673e3a425ca36
RandyRagnar <randytsiory@gmail.com>
2024-10-18 02:28:01 +0300

    change api url to railway

--- src/views/Mangas.vue
+++ src/views/Mangas.vue
@@ -81,7 +81,7 @@ export default defineComponent({
       currentPage.value = pageFromQuery;
 
       try {
-        const response = await axios.get('https://vue-mangas-api.onrender.com/api/mangas');
+        const response = await axios.get('https://vuemangasapi-production.up.railway.app/api/mangas');
         mangas.value = response.data.data;
       } catch (error) {
         console.log('Erreur lors de la récupération des mangas', error);
